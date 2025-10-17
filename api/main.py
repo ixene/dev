0965e85a2c7637dff7dd12c662cb75672ff1b316
ixene <your_email@example.com>
2025-10-17 11:34:52 -0500

    linked frontend and backend, working full stack

--- api/main.py
+++ api/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import files
 
-app = FastAPI()
+app = FastAPI(title="ixene-dev API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
